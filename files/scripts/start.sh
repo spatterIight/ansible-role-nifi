1f50586dd056cb0ef487f2e983e31eab0798f2ed
spatterlight <spatterlight@spatterlight.space>
2025-11-22 01:02:20 -0500

    Issue 1: Initial development

--- files/scripts/start.sh
+++ files/scripts/start.sh
@@ -19,33 +19,6 @@ scripts_dir='/opt/nifi/scripts'
 
 [ -f "${scripts_dir}/common.sh" ] && . "${scripts_dir}/common.sh"
 
-# Set nifi-toolkit properties files and baseUrl
-"${scripts_dir}/toolkit.sh"
-
-. "${scripts_dir}/update_cluster_state_management.sh"
-
-# Check if we are secured or unsecured
-case ${AUTH} in
-    tls)
-        echo 'Enabling Two-Way SSL user authentication'
-        . "${scripts_dir}/secure.sh"
-        ;;
-    ldap)
-        echo 'Enabling LDAP user authentication'
-        # Reference ldap-provider in properties
-        export NIFI_SECURITY_USER_LOGIN_IDENTITY_PROVIDER="ldap-provider"
-
-        . "${scripts_dir}/secure.sh"
-        . "${scripts_dir}/update_login_providers.sh"
-        ;;
-    oidc)
-        echo 'Enabling OIDC user authentication'
-
-        . "${scripts_dir}/secure.sh"
-        . "${scripts_dir}/update_oidc_properties.sh"
-        ;;
-esac
-
 # Continuously provide logs so that 'docker logs' can produce them
 "${NIFI_HOME}/bin/nifi.sh" run &
 nifi_pid="$!"
